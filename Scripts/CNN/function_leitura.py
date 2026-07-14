786b20f1b84da49721c48bcddd78754a3d62182e
Felipe Lauria <fel.lauria@gmail.com>
2026-07-14 18:57:04 -0300

    arq de leitura

---
+++ Scripts/CNN/function_leitura.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def le_imagem():
+    return np.array(plt.imread(''))
+
+
+def le_recplot():
+    return np.array(plt.imread(''))
+
+def le_mtf():
+    return 1
--- Scripts/CNN/resnet.py
+++ Scripts/CNN/resnet.py
@@ -2,5 +2,5 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 import torchvision.models as models
-
+from  function_leitura import le_imagem, le_recplot, le_mtf
 
